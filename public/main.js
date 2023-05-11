ecddce6c626f54ec504ae83e567e3cb1e03dc752
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2023-05-11 19:31:36 -0400

    finished v0

--- public/main.js
+++ public/main.js
@@ -1,12 +1,22 @@
-var modalWrapper = document.getElementById("waitlist-wrapper");
+let modalWrapper = document.getElementById("waitlist-wrapper");
 let modal = document.getElementById("waitlist-modal");
-console.log("here");
 // Get the button that opens the modal
-var btn = document.getElementById("waitlist-modal-button");
-var closeBtn = document.getElementById("close-waitlist-modal");
+let btn = document.getElementById("waitlist-modal-button");
+let navBtn = document.getElementById("nav-waitlist-btn");
+let closeBtn = document.getElementById("close-waitlist-modal");
+
+// configure toast
+toastr.options.positionClass = "toast-top-center";
+toastr.options.progressBar = true;
+toastr.options.showMethod = "slideDown";
+toastr.options.hideMethod = "fadeOut";
+toastr.options.timeOut = 6000;
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
+	modalWrapper.classList.remove("hidden");
+};
+navBtn.onclick = function () {
 	modalWrapper.classList.remove("hidden");
 };
 
@@ -21,3 +31,46 @@ window.onclick = function (event) {
 		modalWrapper.classList.add("hidden");
 	}
 };
+
+// form submission
+
+let form = document.getElementById("waitlist-form");
+async function handleSubmit(event) {
+	event.preventDefault();
+	console.log(event.target);
+	let data = new FormData(event.target);
+
+	fetch(event.target.action, {
+		method: form.method,
+		body: data,
+		headers: {
+			Accept: "application/json",
+		},
+	})
+		.then((response) => {
+			if (response.ok) {
+				toastr.success("Thanks for your submission!");
+				toastr.info(
+					"We'll never spam you, but look forward to a welcome email soon"
+				);
+				form.reset();
+				modalWrapper.classList.add("hidden");
+			} else {
+				response.json().then((data) => {
+					if (Object.hasOwn(data, "errors")) {
+						data["errors"].map((error) => {
+							console.log(`Error: ${error}`);
+						});
+					}
+					toastr.error("Oops, something went wrong");
+					toastr.info("Please shoot us a message at carevaulthealth@gmail.com");
+				});
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			toastr.error("Oops, something went wrong");
+			toastr.info("Please shoot us a message at carevaulthealth@gmail.com");
+		});
+}
+form.addEventListener("submit", handleSubmit);
