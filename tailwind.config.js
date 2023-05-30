1eb4617580f55c78b1dd4c387529dda0cbec3b73
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2023-05-30 09:42:36 -0400

    ui tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ["./public/**/*.html"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			title: ["Montserrat", "sans-serif"],
+			serif: ["Georgia", "Times New Roman", "serif"],
+			mono: ["Consolas", "Monaco", "monospace"],
+		},
+		extend: {
+			colors: {
+				link: "#0febda",
+				textSecondary: "#D3CCCC",
+				dark: "#02000C",
+			},
+		},
 	},
 	plugins: [],
 };
